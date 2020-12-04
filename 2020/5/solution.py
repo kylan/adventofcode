e933fa3d88d89f848cd86592b37367047d90bfec
Kylan Robinson <kylanr@gmail.com>
2020-12-04 06:06:00 -0800

    Add Day 5 starting files; Add README)

---
+++ 2020/5/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def get_input():
+    with open('input.txt', 'r') as f:
+        content = [line.strip() for line in f]
+        return content
+
+entries = get_input()
+
+## Part 1
+answer = None
+
+
+#assert(answer == TODO)
+print(answer)
+
+## Part 2
+answer = None
+
+
+#assert(answer == TODO)
+print(answer)
