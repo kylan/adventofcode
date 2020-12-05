7918659e6b1b53732fe31129880b71f497360149
Kylan Robinson <kylanr@gmail.com>
2020-12-05 15:50:01 -0800

    Day 5 solution

--- 2020/5/solution.py
+++ 2020/5/solution.py
@@ -5,18 +5,37 @@ def get_input():
         content = [line.strip() for line in f]
         return content
 
+def convert(s):
+    val = 0
+    for c in s:
+        val = val << 1
+        if c in ('B', 'R'):
+            val = val | 1
+    return(val)
+
 entries = get_input()
 
+boarding_passes = {}
+
 ## Part 1
-answer = None
+answer = 0
 
+for entry in entries:
+    row = convert(entry[:-3])
+    col = convert(entry[-3:])
+    pass_id = row * 8 + col
+    boarding_passes[pass_id] = (row, col)
+    answer = max(answer, row * 8 + col)
 
-#assert(answer == TODO)
+assert(answer == 915)
 print(answer)
 
 ## Part 2
 answer = None
 
+for key in sorted(boarding_passes.keys()):
+    if key + 1 not in boarding_passes and key + 2 in boarding_passes:
+        answer = key + 1
 
-#assert(answer == TODO)
+assert(answer == 699)
 print(answer)
