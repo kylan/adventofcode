2dcba858bdb3b4d5db8ea307f60d022058a46af1
Kylan Robinson <kylanr@gmail.com>
2020-12-09 22:52:03 -0800

    Day 10 solutions

--- 2020/10/solution.py
+++ 2020/10/solution.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
+from functools import reduce
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="The file with puzzle input")
@@ -17,12 +19,41 @@ def get_input():
         content = [int(line.strip()) for line in f]
         return content
 
+def prepare_list(data):
+    data.sort()
+    data.insert(0, 0)
+    data.append(data[-1]+3)
+    return data
+
+def measure_gaps(data):
+    gaps = [j-i for i, j in zip(data, data[1:])]
+    return gaps
+
 ## Part 1
 answer = None
 
-solution(answer, None)
+distribution = collections.Counter(measure_gaps(prepare_list(get_input())))
+answer = distribution[1] * distribution[3]
+solution(answer, 1625)
 
 ## Part 2
 answer = None
 
-solution(answer, None)
+run_combos = {1:1, 2:2, 3:4, 4:7}
+
+def run_list(data):
+    run_list = []
+    current_run = 0
+    for item in data:
+        if item == 1:
+            current_run += 1
+        else:
+            if current_run > 0:
+                run_list.append(run_combos[current_run])
+            current_run = 0
+    return run_list
+
+gaps = measure_gaps(prepare_list(get_input()))
+runs = run_list(gaps)
+answer = reduce(lambda x, y: x*y, runs)
+solution(answer, 3100448333024)
